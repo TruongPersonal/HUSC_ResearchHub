fadc50645bed25d0db41c845b694afd5c24942fd
Ngô Quang Trường <ngoquangtruong.isme@gmail.com>
2026-01-06 03:43:47 +0700

    Deloy railway

--- backend/src/main/java/vn/edu/husc/researchhub/config/SecurityConfig.java
+++ backend/src/main/java/vn/edu/husc/researchhub/config/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig {
                 cors.configurationSource(
                     request -> {
                       var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                      corsConfiguration.setAllowedOriginPatterns(
-                          java.util.List.of("https://huscresearchhub.truongpersonal.vercel.app"));
+                      corsConfiguration.setAllowedOriginPatterns(java.util.List.of("*"));
                       corsConfiguration.setAllowedMethods(
                           java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                       corsConfiguration.setAllowedHeaders(java.util.List.of("*"));
